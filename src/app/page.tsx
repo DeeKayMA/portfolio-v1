93789d37f00cf5662c3c0c600724f4a17b336183
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-04-22 17:00:59 +0100

    Started Projects Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-
-const sectionPadding = ""
+// import { Inter } from "next/font/google";
+import { ProjectCard } from "@/components/projectCard/projectCard";
 
 export default function Home() {
   return (
-    <div className="flex flex-col lg:flex-row lg:min-h-screen m-0 py-12 px-6 md:py-16 md:px-12 lg:p-24 gap-4" >
+    <div className="flex flex-col lg:flex-row lg:min-h-screen m-0 py-12 px-6 md:py-16 md:px-12 lg:p-24 lg:gap-4 gap-16" >
     {/* This is the navigation part of the page */}
-    <aside aria-label="Sidebar navigation" className="lg:w-1/2 flex flex-col lg:justify-between sticky lg:top-24">
+    <aside aria-label="Sidebar navigation" className="lg:w-1/2 flex flex-col lg:justify-between sticky lg:top-24 gap-4 max-h-[calc(100vh-12rem)]">
       <header className="flex flex-col gap-4">
         <h1 className="text-4xl font-bold">Diquan Kerr</h1>
         <h2 className="text-lg font-medium">Front End Engineer</h2>
@@ -33,15 +33,23 @@ export default function Home() {
     </aside>
 
     {/* This is the main part of the page */}
-    <main className="lg:w-1/2">
-      <section aria-label="About me">
+    <main className="lg:w-1/2 flex flex-col gap-24">
+      <section aria-label="About me" id="about">
+        <h2 className="my-4 lg:hidden">About</h2>
         <p>Hi, I’m Diquan. I began my career as a product manager, but customising WordPress sites in my spare time sparked a genuine passion for UX and front-end development. That interest quickly grew into a hands-on experience with HTML, CSS, JavaScript, React, and Next.js—fuelling my drive to build thoughtful, user-focused solutions. Today, I’m focused on growing as a front-end engineer, combining technical skills with product insight to create meaningful digital experiences.</p>
       </section>
-      <section aria-label="Projetcs">
-        <h2>Projects</h2>
+      <section aria-label="Projetcs" id="projects">
+        <h2 className="my-4 lg:hidden">Projects</h2>
+        <ProjectCard
+          name="Reddit Micro"
+          description="A minimalist Reddit client that lets users browse, search, and view posts and comments in a simplified interface."
+          liveLink="https://redditmicro.netlify.app"
+          githubLink="https://github.com/DeeKayMA/reddit_micro"
+          tools={["React", "CSS", "Reddit API", "Netlify"]}
+        />
       </section>
-      <section aria-label="Experience">
-        <h2>Experience</h2>
+      <section aria-label="Experience" id="experience">
+        <h2 className="my-4 lg:hidden">Experience</h2>
       </section>
       <footer>
         <p>The interface was crafted in Figma and built in Visual Studio Code using Next.js. Styling and subtle animations, powered by Tailwind CSS, bring it to life, while seamless deployment is handled through Vercel. The versatile Inter typeface complements the clean, modern aesthetic.</p>
--- src/components/projectCard/projectCard.tsx
+++ src/components/projectCard/projectCard.tsx
@@ -1,5 +1,47 @@
 import { Badge } from "@/components/ui/badge"
 
-export function BadgeOutline() {
-    return <Badge variant="outline">Outline</Badge>
+type ProjectCardProps = {
+    name: string
+    description: string
+    liveLink: string
+    githubLink: string
+    tools: string[]
+
+  }
+
+export function ProjectCard({ name, description, liveLink, githubLink, tools }: ProjectCardProps) {
+
+
+
+    return (
+    <div className="flex flex-row">
+        <div>
+            {/* Need Image */}
+        </div>
+        <div className="flex flex-col gap-4">
+            {/* Project Name */}
+            <h3 className="font-medium">{name} ↗</h3>
+
+            {/* Project Description */}
+            <p className="text-sm text-muted-foreground">{description}</p>
+
+            {/* Project Links */}
+            <div className="flex flex-row gap-4">
+                <a href={liveLink} target="_blank" rel="noopener noreferrer" className="text-sm ">Live</a>
+                <a href={githubLink} target="_blank" rel="noopener noreferrer" className="text-sm ">GitHub</a>
+            </div>
+
+            {/* Tools */}
+            <div className="flex flex-row gap-2">
+                {tools.map((tool) => (
+                    <Badge key={tool} variant="outline" className="text-sm">{tool}</Badge>
+                ))}
+            </div>
+            
+        </div>
+        
+        
+    </div>
+    )
+    
   }
