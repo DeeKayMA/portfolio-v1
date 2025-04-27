360c56e4b778885e156729091a5f7e0eaddeb0d8
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-04-27 19:01:04 +0100

    Added Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Cursor } from "@/components/cursor/cursor";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
         id="root"
         suppressHydrationWarning={true}
       >
+        <SpeedInsights/>
         <Cursor/>
         {children}
       </body>
