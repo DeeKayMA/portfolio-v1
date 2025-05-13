a814e03a8066e8405d5d3b5d5d072e6a09679e4e
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-05-13 10:08:01 +0100

    Added head for project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Diquan Kerr",
   description: "Diquan Kerr Portfolio - Front End Engineer",
-  icons: "/favicon.ico",
+  icons: "/images/favicon.ico",
 };
 
 export default function RootLayout({
@@ -28,6 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-stone-950 font-inter text-stone-400 selection:bg-amber-400 selection:text-stone-900`}
         id="root"
