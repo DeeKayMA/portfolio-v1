c5f6ea153c661c99c9cecf6fcacc4e93a1f4ab52
DeeKayMA <100499813+DeeKayMA@users.noreply.github.com>
2025-05-16 10:30:57 +0100

    Remove native cursor

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -13,11 +13,8 @@ export const Navbar = () => {
     useEffect(() => {
         const sectionIds = ["about", "projects", "experience", "education", "techstack"];
         const handleScroll = () => {
-            // const scrollPosition = window.scrollY + window.innerHeight * 0.4;
             // const scrollPosition = window.scrollY + window.innerHeight/2; 
-
             const scrollPosition = window.scrollY + window.innerHeight * 0.7; 
-            // const scrollPosition = window.scrollY + 100;
 
             // let found = false;
 
